1f64d4a3ddc4cbdcf947c53892024339464d2e63
Khanya <khanya.keswa@gmail.com>
2022-10-12 17:41:22 -0500

    assignment complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const taskForm = document.getElementById('create-task-form')
+const taskDescription = document.getElementById('new-task-description')
+const taskList = document.getElementById('tasks')
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Dom loaded successfully')
+  function taskSubmit(event) {
+    event.preventDefault()
+    const li = document.createElement('li')
+    if (taskDescription.value === '') {
+      alert("Task empty!");
+      return;
+    } else {
+      li.innerText = taskDescription.value
+      console.log(taskDescription.textContent)
+    }
+    taskList.append(li)
+  }
+  taskForm.addEventListener('submit', taskSubmit)
+})
