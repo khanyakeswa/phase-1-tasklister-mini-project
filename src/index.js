9d137453ee9cc193379535555a52f286d412025c
Khanya <khanya.keswa@gmail.com>
2022-10-13 15:22:11 -0500

    js updates

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const taskForm = document.getElementById('create-task-form')
-const taskDescription = document.getElementById('new-task-description')
+const taskDescription = document.getElementById('create_item')
 const taskList = document.getElementById('tasks')
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Dom loaded successfully')
   function taskSubmit(event) {
     event.preventDefault()
+    console.log(event.target.create_item.value)
     const li = document.createElement('li')
     if (taskDescription.value === '') {
       alert("Task empty!");
